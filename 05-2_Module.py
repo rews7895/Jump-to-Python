08727b91bf1d77d5a9e209ba03c6fa43d4803f68
박준석 <yalvz@naver.com>
2020-09-18 21:22:16 +0900

    05-2 모듈

---
+++ 05-2_Module.py
@@ -0,0 +1,12 @@
+# 모듈
+# 모듈 : 함수나 변수 또는 클래스를 모아 놓은 파일이다.
+# from etc.mod1 import add, sub
+# from etc.mod1 import *
+# print(add(3, 4))
+
+import etc.mod2 as mod2
+result = mod2.add(3, 4)
+print(result)
+
+
+
---
+++ etc/mod1.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+# __name__ : 이 변수는 파이썬이 내부적으로 사용하는 특별한 변수. 
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(add(4, 2))
